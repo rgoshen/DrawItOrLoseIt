faa8eed5ff0a0b5d0bf3caf9dba32c4fe5635162
rgoshen <rick.goshen@gmail.com>
2025-03-23 16:48:50 -0700

    Add passing test for create game with unique game
    Signed-off-by: rgoshen <rick.goshen@gmail.com>
    - GameService: add clear() method to clear tests after test run
    - GaseServiceTest add passing testAddGame_UniqueName()

--- test/com/gamingroom/GameServiceTest.java
+++ test/com/gamingroom/GameServiceTest.java
@@ -1,21 +1,30 @@
 package com.gamingroom;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Unit test for GameService to verify unique name functionality.
+ */
 public class GameServiceTest {
+
 	private GameService gameService;
 
 	@Before
 	public void setUp() {
 		gameService = GameService.getInstance();
+		gameService.clear(); // Reset state for isolated tests
 	}
 
 	@Test
 	public void testAddGame_UniqueName() {
-		Game game = new Game("UniqueGame");
-		assertTrue(gameService.addGame(game));
+		String gameName = "UniqueGame";
+		Game game = gameService.addGame(gameName);
+
+		assertNotNull("Game should not be null when added with a unique name.", game);
+		assertEquals("Game name should match input name.", gameName, game.getName());
 	}
 }
