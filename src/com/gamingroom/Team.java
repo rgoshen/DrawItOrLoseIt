23bfc37c6128fd0f76ee5314834e651693ea5b93
rgoshen <rick.goshen@gmail.com>
2025-03-23 19:22:27 -0700

    testAddPlayerUniqueName not passes
    Signed-off-by: rgoshen <rick.goshen@gmail.com>

--- src/com/gamingroom/Team.java
+++ src/com/gamingroom/Team.java
@@ -1,5 +1,9 @@
 package com.gamingroom;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * A class to hold information about a team. Extends Entity to inherit common id
  * and name functionality.
@@ -7,6 +11,9 @@ package com.gamingroom;
  * @author coce@snhu.edu
  */
 public class Team extends Entity {
+
+	private List<Player> players = new ArrayList<Player>();
+
 	/**
 	 * Constructor with an identifier and name.
 	 *
@@ -16,4 +23,27 @@ public class Team extends Entity {
 	public Team(long id, String name) {
 		super(id, name);
 	}
+
+	/**
+	 * Adds a new player with a unique name to this team.
+	 *
+	 * @param name the name of the new player
+	 * @return the newly added Player, or existing one if duplicate
+	 */
+	public Player addPlayer(String name) {
+		// Check for existing player name using iterator
+		Iterator<Player> iterator = players.iterator();
+		while (iterator.hasNext()) {
+			Player player = iterator.next();
+			if (player.getName().equalsIgnoreCase(name)) {
+				return player;
+			}
+		}
+
+		// If name is unique, create and add new player
+		Player player = new Player((long) players.size() + 1, name); // ID management can be improved later
+		players.add(player);
+		return player;
+	}
+
 }
