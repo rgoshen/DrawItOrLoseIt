447eaa6d1fda84b2c9d2aeab0de02e6a7385130e
rgoshen <rick.goshen@gmail.com>
2025-03-23 19:55:28 -0700

    Add passing testGetPlayerExistingName test
    Signed-off-by: rgoshen <rick.goshen@gmail.com>

--- src/com/gamingroom/Team.java
+++ src/com/gamingroom/Team.java
@@ -46,6 +46,32 @@ public class Team extends Entity {
 		return player;
 	}
 
+	/**
+	 * Retrieves a player by name from this team.
+	 *
+	 * @param name the name to search for
+	 * @return the matching Player, or null if not found
+	 */
+	public Player getPlayer(String name) {
+		Iterator<Player> iterator = players.iterator();
+		while (iterator.hasNext()) {
+			Player player = iterator.next();
+			if (player.getName().equalsIgnoreCase(name)) {
+				return player;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the number of players currently in this team.
+	 *
+	 * This method is intended primarily for testing purposes, to verify that player
+	 * uniqueness is enforced and no duplicates are added. It is package-private to
+	 * limit visibility outside the com.gamingroom package.
+	 *
+	 * @return the number of players in the team
+	 */
 	int getPlayerCount() {
 		return players.size();
 	}
