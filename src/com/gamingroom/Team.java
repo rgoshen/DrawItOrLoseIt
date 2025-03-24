56e1bae9ea36df179370278e45371772aaae1520
rgoshen <rick.goshen@gmail.com>
2025-03-23 18:06:05 -0700

    Refactor Team to extend Entity
    Signed-off-by: rgoshen <rick.goshen@gmail.com>

--- src/com/gamingroom/Team.java
+++ src/com/gamingroom/Team.java
@@ -1,45 +1,19 @@
 package com.gamingroom;
 
 /**
- * A simple class to hold information about a team
- * <p>
- * Notice the overloaded constructor that requires
- * an id and name to be passed when creating.
- * Also note that no mutators (setters) defined so
- * these values cannot be changed once a team is
- * created.
- * </p>
+ * A class to hold information about a team. Extends Entity to inherit common id
+ * and name functionality.
+ * 
  * @author coce@snhu.edu
- *
  */
-public class Team {
-	long id;
-	String name;
-	
-	/*
-	 * Constructor with an identifier and name
+public class Team extends Entity {
+	/**
+	 * Constructor with an identifier and name.
+	 *
+	 * @param id   the unique ID of the team
+	 * @param name the name of the team
 	 */
 	public Team(long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
-	/**
-	 * @return the id
-	 */
-	public long getId() {
-		return id;
-	}
-
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	@Override
-	public String toString() {
-		return "Team [id=" + id + ", name=" + name + "]";
+		super(id, name);
 	}
 }
