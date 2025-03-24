6dce4f757a4b51864c1ccb48f8a718fadf183850
rgoshen <rick.goshen@gmail.com>
2025-03-23 19:15:30 -0700

    Add failing team test unique name
    Signed-off-by: rgoshen <rick.goshen@gmail.com>

---
+++ test/com/gamingroom/TeamTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.gamingroom;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests for the Team class.
+ */
+public class TeamTest {
+
+	private Team team;
+
+	@Before
+	public void setUp() {
+		team = new Team(1, "TestTeam");
+	}
+
+	@Test
+	public void testAddPlayer_UniqueName() {
+		Player player = team.addPlayer("PlayerOne");
+
+		assertNotNull("Player should not be null when added with a unique name.", player);
+		assertEquals("PlayerOne", player.getName());
+	}
+
+}
